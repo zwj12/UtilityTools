8ef77c109b0ae15725a2d0f21629790576e94230
zwj12 <zwj_12@163.com>
2024-04-10 19:58:13 +0800

    update

--- PMTWUserScript/ExternalSensorsTriggerUDPSample.py
+++ PMTWUserScript/ExternalSensorsTriggerUDPSample.py
@@ -9,7 +9,6 @@ def TrigWorkArea(signalName):
     PORT = 3003         
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.connect((HOST, PORT))
-        signalName = "doManSync1"
         dataRequest = struct.pack(f">BHBf{len(signalName)}s", 19, len(signalName) + 5, 0, 0, signalName.encode('utf-8'))
         print(f'Request: {dataRequest.hex()} {dataRequest}')
         s.sendall(dataRequest)
